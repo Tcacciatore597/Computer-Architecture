c64ab3f78de976f664ed2f5f983985a55aaec3e1
Thomas Cacciatore <tcacciatore597@gmail.com>
2019-11-12 17:38:39 -0500

    Updated cpu.py to handle Mult instructions and updated alu func.  Can run different examples in the command line and print correct outputs for print8 and mult.ls8

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -14,6 +14,7 @@ class CPU:
         self.HLT = 0b00000001
         self.PRN = 0b01000111
         self.LDI = 0b10000010
+        self.MUL = 0b10100010
 
     def ram_read(self, MAR):
         value = self.ram[MAR]
@@ -29,26 +30,47 @@ class CPU:
 
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111, # PRN R0
+        #     0b00000000,
+        #     0b00000001, # HLT
+        # ]
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        # for instruction in program:
+        #     self.ram[address] = instruction
+        #     address += 1
 
+        if len(sys.argv) != 2:
+            print("usage: ls8.py filename")
+            sys.exit(1)
+
+        progname = sys.argv[1]
+
+        with open(progname) as f:
+            for line in f:
+                line = line.split("#")[0]
+                line = line.strip()
+                
+                if line == '':
+                    continue
+
+                val = int(line, 2)
+                # print(val)
+
+                self.ram[address] = val
+                address += 1
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
+        elif op == self.MUL:
+            self.reg[reg_a] *= self.reg[reg_b]
         #elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
@@ -88,7 +110,7 @@ class CPU:
                 self.pc += 3
             # elif IR == OPCODES.NOP.code:
             #     self.pc += 1
-            elif  IR == self.PRN:
+            elif IR == self.PRN:
                 # Print numeric value stored in the given register.
                 # Print to the console the decimal integer value that is stored in the given
                 # register.
@@ -96,6 +118,9 @@ class CPU:
                 value = int(self.reg[reg_index])
                 print(f'{value}')
                 self.pc += 2
+            elif IR == self.MUL:
+                self.alu(self.MUL, operand_a, operand_b)
+                self.pc += 3
             elif IR == self.HLT:
                 halted = True
                 self.pc += 1
